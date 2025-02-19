2aa5fd1f6a1f4a73d33bb8550a444884d9dd86f5
Terkozmoz <126075332+Terkozmoz@users.noreply.github.com>
2025-02-19 16:13:35 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -6,4 +6,5 @@ curl -s https://raw.githubusercontent.com/Terkozmoz/aliases/main/aliases -o ~/al
 # Add source to .zshrc
 echo 'source ~/aliases' >> ~/.zshrc
 
-echo "Setup complete! Please restart your terminal or run 'source ~/.zshrc' to apply changes."
+echo "Setup complete!"
+source ~/.zshrc
