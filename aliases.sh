df50035e23817d696736760ef776192a68da317f
Terkozmoz <126075332+Terkozmoz@users.noreply.github.com>
2025-02-19 16:15:17 +0100

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Download aliases file
-curl -s https://raw.githubusercontent.com/Terkozmoz/aliases/main/aliases -o ~/aliases
+curl -s https://raw.githubusercontent.com/Terkozmoz/aliases/main/.aliases -o ~/.aliases
 
 # Add source to .zshrc
-echo 'source ~/aliases' >> ~/.zshrc
+echo 'source ~/.aliases' >> ~/.bashrc
 
 echo "Setup complete!"
-source ~/.zshrc
